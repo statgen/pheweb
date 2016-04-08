f272aa4a0583116539569d585d2cec58678d6392
Peter VandeHaar <pjvandehaar@gmail.com>
2016-04-08 02:24:29 -0400

    Don't bother tabixing the sites vcf

--- data/1_3_sites_only.sh
+++ data/1_3_sites_only.sh
@@ -4,6 +4,4 @@ set -eou pipefail
 
 /net/mario/cluster/bin/pigz -dc "/var/pheweb_data/phewas_maf_gte_1e-2_ncases_gte_20.vcf.gz" |
 cut -d $'\t' -f 1,2,3 |
-/net/mario/cluster/bin/bgzip > "/var/pheweb_data/phewas_maf_gte_1e-2_ncases_gte_20_sites.vcf.gz"
-
-/net/mario/cluster/bin/tabix "/var/pheweb_data/phewas_maf_gte_1e-2_ncases_gte_20_sites.vcf.gz"
+/net/mario/cluster/bin/pigz > "/var/pheweb_data/phewas_maf_gte_1e-2_ncases_gte_20_sites.vcf.gz"
