5db1b7cc17f7187f9b4fd9f8dc75dd83bd54adec
Peter VandeHaar <pjvandehaar@gmail.com>
2016-04-11 02:45:13 -0400

    Rename/remove scripts

--- data-old/1_create_tables.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export PGPASSWORD="$(cat postgres_password)"
-
-psql -U pheweb_writer -d postgres -h localhost < schema.sql
--- data-old/3_load_associations.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-export PGPASSWORD="$(cat postgres_password)"
-
-./print_associations.py |
-psql -U pheweb_writer -d postgres -h localhost -c "COPY pheweb.associations (variant_id, pheno_id, beta, sebeta, maf, pval) FROM STDIN WITH DELIMITER ' ';"
