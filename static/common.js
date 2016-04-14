867f7471d8d1c850e659cbb936e1ad6f0e5d3819
Peter VandeHaar <pjvandehaar@gmail.com>
2016-04-13 23:45:33 -0400

    Store previously-seen autocomplete suggestions for immediate suggestion by Bloodhound

--- static/common.js
+++ static/common.js
@@ -1,18 +1,32 @@
 $(function() {
-    var autocomplete_bloodhound = new Bloodhound({
+    var defaults = [{"display":"1:16344683-C-T","url":"/variant/1:16344683-C-T","value":"1:16344683-C-T"},{"display":"340 (Migraine)","url":"/pheno/340","value":"340"},{"display":"Polyneuropathy due to drugs (316.1)","url":"/pheno/316.1","value":"316.1"}]
+
+    autocomplete_bloodhound = new Bloodhound({
         datumTokenizer: Bloodhound.tokenizers.obj.whitespace('display'),
         queryTokenizer: Bloodhound.tokenizers.whitespace,
         identify: function(sugg) { return sugg.display; }, // maybe allows Bloodhound to `.get()`  objects
         remote: {
             url: '/api/autocomplete?query=%QUERY',
-            wildcard: '%QUERY'
+            wildcard: '%QUERY',
+            rateLimitBy: 'throttle',
+            rateLimitWait: 500,
+            transform: function(data) {
+                // Probably this function reveals that I don't understand Bloodhound.
+                // But, I want my previous results to stay around while I keep typing.
+                // If the string that's currently in the searchbox matches some string that's been suggested before, I want to see it!
+                // This especially happens while I'm typing a chrom-pos-ref-alt.  If what I'm typing agrees with something being suggested, it shouldn't disappear!
+                // So, I'm just adding everything to the local index. (Note: NOT localstorage.)
+                // Bloodhound appears to perform deduping.
+                autocomplete_bloodhound.add(data);
+                return data;
+            },
         },
-        local: [{"display":"1:16344683-C-T","url":"/variant/1:16344683-C-T","value":"1:16344683-C-T"},{"display":"340 (Migraine)","url":"/pheno/340","value":"340"},{"display":"Polyneuropathy due to drugs (316.1)","url":"/pheno/316.1","value":"316.1"}]
+        local: defaults,
     });
 
     function autocomplete_bloodhound_with_defaults(query, sync, async) {
       if (query.trim() === '') {
-        sync(autocomplete_bloodhound.all());
+        sync(defaults);
       } else {
         autocomplete_bloodhound.search(query, sync, async);
       }
