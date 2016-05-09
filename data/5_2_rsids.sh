62c15685ef034abe364bca14ee8553ef7827bdea
Peter VandeHaar <pjvandehaar@gmail.com>
2016-05-09 12:02:58 -0400

    Build a trie of chrom-pos-ref-alt -> rsid and vice-versa

---
+++ data/5_2_rsids.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -euo pipefail
+
+data_dir="/var/pheweb_data/"
+
+if ! [[ -e "$data_dir/dbSNP/dbsnp-b147-GRCh37.gz" ]]; then
+    echo downloading!
+#    wget -O "$data_dir/dbSNP/dbsnp-b147-GRCh37.gz" "ftp://ftp.ncbi.nlm.nih.gov/snp/organisms/human_9606_b147_GRCh37p13/database/organism_data/b147_SNPChrPosOnRef_105.bcp.gz"
+    wget -O "$data_dir/dbSNP/dbsnp-b147-GRCh37.gz" "ftp://ftp.ncbi.nlm.nih.gov/snp/organisms/human_9606_b147_GRCh37p13/VCF/All_20160408.vcf.gz"
+fi
+
+# set +e
+gzip -cd "$data_dir/dbSNP/dbsnp-b147-GRCh37.gz" |
+# head -n 1000 | # causes err 141: signal SIGPIPE from gunzip.
+perl -F'\t' -nale 'print "$F[0]\t$F[1]\t$F[2]\t$F[3]\t$F[4]"' | # Gotta declare that it's tab-delimited, else it's '\s'-delimited I think.
+grep '^[0-9]' | # We only have chr1-22, so throw away X, Y, MT, etc.
+gzip > "$data_dir/dbSNP/rsids.vcf.gz"
+# set -e
+
+echo done!
---
+++ data/5_4_trie_sites.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python2
+
+from __future__ import print_function, division, absolute_import
+
+import os.path
+
+activate_this = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../venv/bin/activate_this.py')
+execfile(activate_this, dict(__file__=activate_this))
+
+import marisa_trie
+import gzip
+
+data_dir = '/var/pheweb_data/'
+
+def parse_line(line):
+    chrom, pos, ref, alt, rsid = line.rstrip('\n').split('\t')
+    # Keys in marisa_trie must be unicode. Values in BytesTrie must be bytes.
+    return (u'{}-{}-{}-{}'.format(chrom, pos, ref, alt), rsid)
+
+with gzip.open(data_dir + '/phewas_maf_gte_1e-2_ncases_gte_20_sites_rsids.vcf.gz') as f:
+    lines = [parse_line(line) for line in f]
+print('done loading.')
+
+sites_rsids_trie = marisa_trie.BytesTrie(lines)
+out_filename = data_dir + '/sites_rsids_trie.marisa'
+sites_rsids_trie.save(out_filename)
+print('done with chrom-pos-ref-alt -> rsid trie at ' + out_filename)
+
+# for chrom_pos_ref_alt, rsids in lines:
+#     if ',' in rsids:
+#         print(rsids)
+
+rsids_sites_trie = marisa_trie.BytesTrie((rsid.decode('ascii'), chrom_pos_ref_alt.encode('ascii')) for (chrom_pos_ref_alt, rsids) in lines for rsid in rsids.split(','))
+out_filename = data_dir + '/rsids_sites_trie.marisa'
+rsids_sites_trie.save(out_filename)
+print('done with rsid -> chrom-pos-ref-alt trie at ' + out_filename)
