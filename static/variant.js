10540d3f420456654f7aec1590cd6b7e988899c5
Peter VandeHaar <pjvandehaar@gmail.com>
2016-05-13 15:35:25 -0400

    Check for results in PubMed before showing a link to it

--- static/variant.js
+++ static/variant.js
@@ -219,28 +219,40 @@
 })();
 
 
-(function() { // Check PubMed for each rsid and render link.
-    var pubmed_api_template = _.template('http://www.ncbi.nlm.nih.gov/pubmed/?term=<%= rsid %>&format=text');
-    var pubmed_link_template = _.template('http://www.ncbi.nlm.nih.gov/pubmed/?term=<%= rsid %>');
-    var rsids = window.variant.rsids.split(','); // There's usually just one rsid.
-    rsids.forEach(function(rsid) {
-        var pubmed_api_url = pubmed_api_template({rsid: rsid});
-        var pubmed_link_url = pubmed_link_template({rsid: rsid});
+if (typeof window.variant.rsids !== "undefined") {
+    (function() { // Check PubMed for each rsid and render link.
+        // var pubmed_api_template = _.template('http://www.ncbi.nlm.nih.gov/pubmed/?term=<%= rsid %>&format=text');
+        // This url has `Access-Control-Allow-Origin` so that I can access it:
+        //var pubmed_api_template = _.template('http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&retmax=1&retmode=xml&tool=eutils-test&term=<%= rsid %>');
+        var pubmed_api_template = _.template('http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&retmax=1&retmode=xml&term=<%= rsid %>');
+        var pubmed_link_template = _.template('http://www.ncbi.nlm.nih.gov/pubmed/?term=<%= rsid %>');
+        var rsids = window.variant.rsids.split(','); // There's usually just one rsid.
+        rsids.forEach(function(rsid) {
+            var pubmed_api_url = pubmed_api_template({rsid: rsid});
+            var pubmed_link_url = pubmed_link_template({rsid: rsid});
 
-        $.getJSON(pubmed_api_url).done(function(result) {
-            var has_pubmed_results = _.any(result.split('\n'), function(line) { return line[0] !== '<'; });
-            if (has_pubmed_results) {
-                if (rsids.length == 1) {
-                    $('#pubmed-link').html(', <a href="{URL}" target="_blank">PubMed</a>'
-                                           .replace('{URL}', pubmed_link_url));
-                    console.log(rsid);
-                } else {
-                    $('#pubmed-link').html(', <a href="{URL}" target="_blank">PubMed for {RSID}</a>'
-                                           .replace('{URL}', pubmed_link_url)
-                                           .replace('{RSID}', rsid));
-                    console.log(rsid);
+            $.ajax({
+                url: pubmed_api_url,
+                datatype: 'xml',
+            }).done(function(result) {
+                // window.rv = result;
+                var count_elem = result.querySelector('eSearchResult Count');
+                var num_pubmed_results = (count_elem === null) ? 0 : Number.parseInt(count_elem.textContent);
+                if (num_pubmed_results > 0) {
+                    if (rsids.length == 1) {
+                        $('#pubmed-link').html(', <a href="{URL}" target="_blank">PubMed ({NUM} results)</a>'
+                                               .replace('{URL}', pubmed_link_url)
+                                               .replace('{NUM}', num_pubmed_results));
+                    } else {
+                        $('#pubmed-link').html(', <a href="{URL}" target="_blank">PubMed for {RSID} ({NUM} results)</a>'
+                                               .replace('{URL}', pubmed_link_url)
+                                               .replace('{RSID}', rsid)
+                                               .replace('{NUM}', num_pubmed_results));
+                    }
                 }
-            }
+            }).fail(function(obj) {
+                console.log(['failed!', obj]);
+            });
         });
-    });
-})();
+    })();
+}
