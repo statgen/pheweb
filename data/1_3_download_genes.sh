be455f09390798c302b090c61c4fe833edaffbd3
Peter VandeHaar <pjvandehaar@gmail.com>
2016-06-28 14:25:01 -0400

    Include only protein-coding genes, and a few other kinds

--- data/1_3_download_genes.sh
+++ data/1_3_download_genes.sh
@@ -16,8 +16,9 @@ if ! [[ -e "$data_dir/sites/genes/genes.lexicographic.bed" ]]; then
         wget -O "$data_dir/sites/genes/gencode.gtf.gz" "ftp://ftp.sanger.ac.uk/pub/gencode/Gencode_human/release_19/gencode.v19.annotation.gtf.gz"
     fi
 
-    # TODO: these mostly have `gene_type` of `protein_coding`, `pseudogene`, `lincRNA`, `antisense`, or `miRNA`.  Should I filter on that category?
     gzip -cd "$data_dir/sites/genes/gencode.gtf.gz" |
+    # Remove pseudogenes and other unwanted types of genes.
+    grep -E '; gene_type "(protein_coding|IG_V_gene|TR_V_gene|TR_J_gene|IG_C_gene|IG_D_gene|IG_J_gene|TR_C_gene|TR_D_gene)";' |
     # Remove `chr` from the beginning of the lines and print out `chr startpos endpos genename`.
     perl -F'\t' -nale '$F[0] =~ s{chr}{}; print "$F[0]\t$F[3]\t$F[4]\t", m{gene_name "(.*?)";} if $F[2] eq "gene"' \
     > "$data_dir/sites/genes/genes.bed"
