dc290cdaf5de5e05e6bfa9b6b4a3b8ba6ab210b5
Peter VandeHaar <pjvandehaar@gmail.com>
2016-07-12 12:21:00 -0400

    Show toolbar tooltips

--- static/region.js
+++ static/region.js
@@ -4,20 +4,18 @@
     var remoteBase = "http://portaldev.sph.umich.edu/api/v1/";
     var data_sources = new LocusZoom.DataSources();
     data_sources.add("base", ["AssociationLZ", localBase]);
-//    data_sources.add("ld", ["LDLZ" ,remoteBase + "pair/LD/"]);
+    data_sources.add("ld", ["LDLZ" ,remoteBase + "pair/LD/"]);
     data_sources.add("gene", ["GeneLZ", { url: remoteBase + "annotation/genes/", params: {source: 2} }]);
     data_sources.add("sig", ["StaticJSON", [{ "x": 0, "y": 4.522 }, { "x": 2881033286, "y": 4.522 }] ])
     data_sources.add("recomb", ["RecombLZ", { url: remoteBase + "annotation/recomb/results/", params: {source: 15} }])
 
-    //data_sources.add("recomb", ["RecombLZ", { url: apiBase + "annotation/recomb/results/", params: {source: 15}])
     var my_layout = {
         resizable: "responsive",
         panels: {
             positions: {
                 data_layers: {
                     positions: {
-//                        fields: ["id", "position", "rsid", "pvalue|scinotation", "pvalue|neglog10", "maf", "ld:state"],
-                        fields: ["id", "chr", "position", "ref", "alt", "rsid", "pvalue|scinotation", "pvalue|neglog10", "maf"],
+                        fields: ["id", "chr", "position", "ref", "alt", "rsid", "pvalue|scinotation", "pvalue|neglog10", "maf", "ld:state"],
                         id_field: "id",
                         tooltip: {
                             html: "<strong>{{id}}</strong><br>" +
