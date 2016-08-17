9e8359b407e3edefea485aa650065c1542fdfd8a
Peter VandeHaar <pjvandehaar@gmail.com>
2016-08-17 15:04:19 -0400

    Show nearest_genes in 9_1_get_all_peaks

--- data/9_1_get_all_peaks.py
+++ data/9_1_get_all_peaks.py
@@ -26,22 +26,23 @@ import json
 with open(my_dir + '/phenos.json') as f:
     phenos = json.load(f)
 
+#phenos = dict(list(phenos.items())[:10]) # debugging
+
 hits_by_chrom = dict()
 for pheno_code in phenos:
     with open(conf.data_dir + '/manhattan/{}.json'.format(pheno_code)) as f:
         j = json.load(f)
     for v in j['unbinned_variants']:
         if v['pval'] <= PVAL_CUTOFF:
-            hits_by_chrom.setdefault(v['chrom'], []).append((v['pos'], v['pval'], pheno_code))
+            hits_by_chrom.setdefault(v['chrom'], []).append((v['pos'], v['pval'], v['nearest_genes'], pheno_code))
 
-winners = []
+top_hits = []
 for chrom, hits in hits_by_chrom.items():
     while hits:
-        best_hit_pos, best_hit_pval, pheno_code = min(hits, key=lambda hit: hit[1])
-        winners.append((chrom, best_hit_pos, best_hit_pval, pheno_code))
+        best_hit_pos, best_hit_pval, nearest_genes, pheno_code = min(hits, key=lambda hit: hit[1])
+        top_hits.append((chrom, best_hit_pos, best_hit_pval, nearest_genes, pheno_code))
         hits = [hit for hit in hits if abs(hit[0] - best_hit_pos) > DISTANCE_BETWEEN_HITS]
 
-with open('winners.tsv', 'w') as f:
-    for winner in sorted(winners, key=(lambda hit: 1e12*int(hit[0]) + hit[1])):
-        f.write('{}\t{}\t{}\t{}\t{pheno_string}\n'.format(*winner, pheno_string=phenos[winner[3]]['phewas_string']))
-
+with open('top-hits.tsv', 'w') as f:
+    for top_hit in sorted(top_hits, key=(lambda hit: 1e12*int(hit[0]) + hit[1])):
+        f.write('{}\t{}\t{}\t{}\t{}\t{pheno_string}\n'.format(*top_hit, pheno_string=phenos[top_hit[4]]['phewas_string']))
