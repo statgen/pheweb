c46efa4ce63eb745923695a2bd4b4cb5f315361b
Peter VandeHaar <pjvandehaar@gmail.com>
2017-01-26 02:33:32 -0500

    Fix bug

--- setup.py
+++ setup.py
@@ -1,19 +1,26 @@
 
 # run this with `python2 setup.py install` (in a virtualenv, probably)
 # while developing, use `python2 setup.py develop` to just make a symlink from `sites-packages` to this folder.
+# to upload to pypi, `python2 setup.py sdist bdist_wheel; twine upload dist/*`
 
 from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
 
 setup(
     name='PheWeb',
     version='0.9',
     description="A tool for building PheWAS websites from association files",
-    long_description=open('README.md', 'rb').read().decode('utf-8'),
+    long_description=readme(),
     author="Peter VandeHaar",
     author_email="pjvh@umich.edu",
     url="https://github.com/statgen/pheweb",
     packages=['pheweb'],
     scripts=['bin/pheweb'],
+    # TODO: use: entry_points={'console_scripts': ['pheweb=pheweb.command_line:main']},
+    #       see <https://python-packaging.readthedocs.io/en/latest/everything.html>
     # TODO: add test_suite (ie, make a single file that runs tests, figure out how to access input_data, make a data_dir in /tmp)
     license="MIT",
     classifiers=[
