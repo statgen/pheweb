df3589cca8766e84c17224cac6c6cc3b05934685
Peter VandeHaar <pjvandehaar@gmail.com>
2017-01-30 08:43:25 -0500

    Make phenolist extract-phenocode-from-fname --simple more clever (and less simple)

--- pheweb/load/phenolist.py
+++ pheweb/load/phenolist.py
@@ -500,7 +500,7 @@ def run(argv):
     @modifies_phenolist
     def f(args, phenolist):
         if args.simple:
-            args.pattern = r'.*/([^/]+?)(?:\.[^/]*)?$'
+            args.pattern = r'.*/(?:epacts-|pheno-)?([^/]+?)(?:\.[^/]*)?$'
         if not args.pattern: utils.die("You must either supply a pattern or use --simple")
         extract_phenocode_from_fname(phenolist, args.pattern)
     p = subparsers.add_parser('extract-phenocode-from-fname', help='use a regex to extract phenocodes from association filenames')
