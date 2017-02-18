37a997d0c5102d634400c1621ad5bc9df01bc7df
Peter VandeHaar <pjvandehaar@gmail.com>
2017-02-17 19:16:56 -0500

    Add categories to tests

--- test/data_dir/make_phenolist.sh
+++ test/data_dir/make_phenolist.sh
@@ -5,9 +5,14 @@ set -x
 
 p='pheweb phenolist'
 
-$p glob --simple-phenocode ../input_files/*
+$p glob --simple-phenocode ../input_files/assoc-files/*
 $p read-info-from-association-files
 $p hide-small-numbers-of-samples --minimum-visible-number 50
 $p filter-phenotypes --minimum-num-cases 20 --minimum-num-controls 20 --minimum-num-samples 20
+
+$p import-phenolist -f "pheno-list-categories.json" "../input_files/categories.csv"
+$p merge-in-info "pheno-list-categories.json"
+$p verify --required-columns category
+
 $p verify
 }
