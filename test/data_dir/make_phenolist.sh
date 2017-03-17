2a14eb43a202f24758ba5fe67125f3f8341f195c
Peter VandeHaar <pjvandehaar@gmail.com>
2017-03-17 18:39:15 -0400

    Do `unique-phenocode` to handle 2-file test pheno

--- test/data_dir/make_phenolist.sh
+++ test/data_dir/make_phenolist.sh
@@ -6,6 +6,7 @@ set -x
 p='pheweb phenolist'
 
 $p glob --simple-phenocode ../input_files/assoc-files/*
+$p unique-phenocode
 $p read-info-from-association-files
 $p hide-small-numbers-of-samples --minimum-visible-number 50
 $p filter-phenotypes --minimum-num-cases 20 --minimum-num-controls 20 --minimum-num-samples 20
