33eb1d809ec5e8cc7e68a1a422ad31b82cfb129c
Peter VandeHaar <pjvandehaar@gmail.com>
2017-04-04 17:07:08 -0400

    Clean up matrixify.cpp

--- pheweb/load/matrix/matrixify.cpp
+++ pheweb/load/matrix/matrixify.cpp
@@ -11,10 +11,9 @@
 
 // Compile with -std=c++11 -lz
 // test with:
-// clear&& g++ -std=c++11 -lz matrixify.cpp &&echo --&& ./a.out 4 ~/PROJECTS/pheweb/test/data_dir/sites/sites.tsv '/Users/peter/PROJECTS/pheweb/test/data_dir/augmented_pheno/*' ~/PROJECTS/pheweb/test/data_dir/matrix.tsv.gz &&echo --&& zcat /Users/peter/PROJECTS/pheweb/test/data_dir/matrix.tsv.gz | head
+// clear&& g++ -Wall -Wextra -std=c++11 -lz matrixify.cpp &&echo --&& ./a.out 4 ~/PROJECTS/pheweb/test/data_dir/sites/sites.tsv '/Users/peter/PROJECTS/pheweb/test/data_dir/augmented_pheno/*' ~/PROJECTS/pheweb/test/data_dir/matrix.tsv.gz &&echo --&& zcat /Users/peter/PROJECTS/pheweb/test/data_dir/matrix.tsv.gz | head
 
 
-#include <iterator>
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -27,10 +26,13 @@
 #include <stdio.h>
 #include <errno.h>
 #include <iomanip> // setprecision
+#include <zlib.h>
+#include <fcntl.h> // O_WRONLY &c
+//#include <sys/stat.h> // struct stat // TODO: should we be using stat to get the system's preferred blocksize?
 
 
 // ------
-// BGZIP-writing code
+// BGZIP-writer
 
 // references:
 // - htslib-cffi (for ffibuilder) @ <https://github.com/quinlan-lab/hts-python/blob/master/hts/hts_concat.h>
@@ -38,13 +40,6 @@
 // todo: understand tabix and build .tbi on-the-fly.
 //       - see <https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3042176/>
 //       - see <https://samtools.github.io/hts-specs/tabix.pdf>
-
-//#include <iostream> // redundant
-//#include <fstream> // redundant
-//#include <sys/stat.h> // struct stat // TODO: should we be using stat to get the system's preferred blocksize?
-#include <zlib.h>
-#include <assert.h>
-#include <fcntl.h> // O_WRONLY &c
 
 class BgzipWriter {
 // This is adapted from <https://github.com/samtools/htslib/blob/master/bgzf.c>,
@@ -66,7 +61,7 @@ public:
     ~BgzipWriter() { close(); }
     void write(const char* src_buffer, size_t src_len) {
         while (src_len > 0) {
-            int copy_length = BGZF_BLOCK_SIZE - _uncompressed_block_size;
+            size_t copy_length = BGZF_BLOCK_SIZE - _uncompressed_block_size;
             if (copy_length > src_len) {
                 copy_length = src_len;
             }
@@ -192,30 +187,27 @@ private:
 };
 
 
+// ------
 // Line-by-line file-reader
+
 class LineReader {
 public:
-    ~LineReader() {
-        delete stream;
-    }
-    inline void attach(std::istream* stream_) {
-        stream = stream_;
+    inline void attach(std::string fpath) {
+        stream.open(fpath);
         next();
     }
     inline void next() {
-        std::getline(*stream, line); // drops the \n
-        // CR remover from <http://stackoverflow.com/a/2529011/1166306>
-        if (!line.empty() && line[line.size() - 1] == '\r') line.erase(line.size() - 1);
+        std::getline(stream, line); // drops the \n
+        if (!line.empty() && line[line.size() - 1] == '\r') line.erase(line.size() - 1); // CR remover from <http://stackoverflow.com/a/2529011/1166306>
     }
-    inline bool eof() {
-        return stream->peek() == std::ifstream::traits_type::eof();
-    }
+    inline bool eof() { return stream.peek() == std::ifstream::traits_type::eof(); }
     std::string line;
-    std::istream* stream;
+    std::ifstream stream;
 };
 
+
 // ------
-// &c
+// utility functions
 
 static inline void set_ulimit_num_files(unsigned num_files) {
   struct rlimit limit;
@@ -240,6 +232,7 @@ static inline std::vector<std::string> glob(const std::string& pat) {
 }
 
 static inline size_t pos_after_n_of_char(std::string str, size_t n, char c) {
+    // return the position AFTER `n` instances of the char `c`
     size_t i = 0;
     for (; i < str.size(); i++) {
         if (n == 0) return i;
@@ -250,6 +243,7 @@ static inline size_t pos_after_n_of_char(std::string str, size_t n, char c) {
 }
 
 static inline size_t n_fields(std::string str) {
+    // count the number of tab-delimited fields in a string
     return 1 + std::count(str.begin(), str.end(), '\t'); // `1+` because there's no trailing \t
 }
 
@@ -258,36 +252,30 @@ static inline size_t n_fields(std::string str) {
 // main
 
 int run(char *sites_fname, char *augmented_pheno_glob, char *matrix_fname) {
-    static const size_t CHR_COL = 0;
-    static const size_t POS_COL = 1;
-    static const size_t REF_COL = 2;
-    static const size_t ALT_COL = 3;
-
     BgzipWriter writer(matrix_fname);
 
     LineReader sites_reader;
-    sites_reader.attach(new std::ifstream(sites_fname));
+    sites_reader.attach(sites_fname);
 
     std::vector<std::string> aug_fpaths = glob(augmented_pheno_glob);
     size_t N_phenos = aug_fpaths.size();
-    std::vector<std::string> aug_fnames(N_phenos);
     std::vector<LineReader> aug_readers(N_phenos);
-    std::vector<int> aug_n_per_assoc_fields(N_phenos); // initialized to 0s.
+    std::vector<std::string> aug_fnames(N_phenos);
+    std::vector<unsigned> aug_n_per_assoc_fields(N_phenos); // initialized to 0s.
     set_ulimit_num_files(N_phenos + 10000); // are python files still open?
     for (size_t i = 0; i < N_phenos; i++) {
+        aug_readers[i].attach(aug_fpaths[i]);
         aug_fnames[i] = aug_fpaths[i];
         size_t last_slash_idx = aug_fnames[i].find_last_of("/");
         if (std::string::npos != last_slash_idx) {
           aug_fnames[i].erase(0, last_slash_idx + 1);
         }
-        std::ifstream* f = new std::ifstream(aug_fpaths[i].c_str());
-        aug_readers[i].attach(f);
     }
 
     // Headers:
-    // Every file's header must begin with "chrom pos ref alt".
+    // Every file's header must begin with "chrom pos ref alt ".
     // Every file's header must begin with the header of sites.tsv
-    // All fields after that should be written as "<field>@<pheno>"
+    // All fields after the ones in sites.tsv will be written as "<field>@<pheno>"
     static const std::string cpra_header = "chrom\tpos\tref\talt\t";
     assert(0 == sites_reader.line.compare(0, cpra_header.size(), cpra_header));
     for (size_t i=0; i < N_phenos; i++) {
@@ -296,12 +284,10 @@ int run(char *sites_fname, char *augmented_pheno_glob, char *matrix_fname) {
     writer.write(sites_reader.line); // no trailing \t or \n
     for (size_t i=0; i < N_phenos; i++) {
         std::string per_assoc_fields = aug_readers[i].line.substr(sites_reader.line.size(), std::string::npos);
-        // std::cout << "paf: [" << per_assoc_fields << "]" << std::endl;
         std::istringstream line_stream(per_assoc_fields);
         std::string field;
         std::getline(line_stream, field, '\t'); // consume first tab.
         while(std::getline(line_stream, field, '\t')) {
-            // std::cout << "field: [" << field << "]" << std::endl;
             writer.write("\t");
             writer.write(field);
             writer.write("@");
@@ -311,44 +297,33 @@ int run(char *sites_fname, char *augmented_pheno_glob, char *matrix_fname) {
     }
     writer.write("\n");
     const size_t n_per_variant_fields = n_fields(sites_reader.line);
+    // advance every file to its 1st data-line
     sites_reader.next();
     for (size_t i=0; i<N_phenos; i++) aug_readers[i].next();
 
     // Data:
-    size_t i_site=0;
+    // Every aug_pheno is a subsequence of sites.tsv.
+    // If a line in an aug_pheno has the same chrom-pos-ref-alt as sites.tsv, then it must have the sites.tsv line as its prefix.
+    //    (ie, it must have the same per-variant fields, in the same order.)
+    // So, we iterate over sites.tsv, printing and advancing any aug_pheno that matches CPRA, and printing '' for every field in non-matching aug_phenos.
     while(1) {
         writer.write(sites_reader.line);
 
         size_t pos_after_cpra = pos_after_n_of_char(sites_reader.line, 4, '\t');
 
-        std::string debugline = "1\t869334\t";
-        bool debug = (0 == sites_reader.line.compare(0, debugline.size(), debugline));
-        if (debug) {
-            std::cout << "cpra: [" << sites_reader.line.substr(0, pos_after_cpra) << "]" << std::endl;
-        }
-
         for (size_t i=0; i<N_phenos; i++) {
-            if (debug)
-                std::cout << "comparing cpra [" << aug_readers[i].line.substr(0, pos_after_cpra) << "] for pheno [" << aug_fnames[i] << "] with " << aug_n_per_assoc_fields[i] << " per-assoc fields" << std::endl;
- 
             if (!aug_readers[i].eof() && 0 == sites_reader.line.compare(0, pos_after_cpra, aug_readers[i].line, 0, pos_after_cpra)) { // CPRAs match.
                 assert(0 == aug_readers[i].line.compare(0, sites_reader.line.size(), sites_reader.line)); // per-variant fields match.
-                assert(n_fields(aug_readers[i].line) == n_per_variant_fields + aug_n_per_assoc_fields[i]);
-                if (debug) {
-                    std::string per_assoc_values = aug_readers[i].line.substr(sites_reader.line.size(), std::string::npos);
-                    std::cout << "- match! - rest of line: [" << per_assoc_values << "]" << std::endl;
-                    writer.write(per_assoc_values); // TODO: avoid intermediate std::string by using aug_readers[i].line.c_str()
-                } else {
-                    writer.write(aug_readers[i].line.c_str() + sites_reader.line.size(), aug_readers[i].line.size() - sites_reader.line.size());
-                }
+                assert(n_fields(aug_readers[i].line) == n_per_variant_fields + aug_n_per_assoc_fields[i]); // correct number of fields on line.
+                writer.write(aug_readers[i].line.c_str() + sites_reader.line.size(), aug_readers[i].line.size() - sites_reader.line.size()); //write per-assoc fields
                 aug_readers[i].next();
 
             } else { // CPRAs don't match
-                for (size_t j=0; j<aug_n_per_assoc_fields[i]; j++) writer.write("\t."); // TODO remove '.'
+                // write blanks for this pheno
+                for (size_t j=0; j<aug_n_per_assoc_fields[i]; j++) writer.write("\t");
             }
         }
         writer.write("\n");
-        // if (i_site++ > 10) break;
 
         if (sites_reader.eof()) break;
         sites_reader.next();
