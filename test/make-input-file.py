045121d5f473f27d11602f030b7191c026d81825
Peter VandeHaar <pjvandehaar@gmail.com>
2017-04-11 17:43:00 -0400

    Add new test assoc files

--- test/make-input-file.py
+++ test/make-input-file.py
@@ -9,24 +9,60 @@ import csv
 import requests
 import random
 import re
+import itertools
 
 def format_float(x):
+    if x >= 0.01: return f'{x:.3}'
     rv = '{:.0e}'.format(x).replace('e-0', 'e-').replace('e+00', '')
     if re.match(r'^[0-9]e-1$', rv): return '.{}'.format(rv[0])
     if re.match(r'^[0-9]e-2$', rv): return '.0{}'.format(rv[0])
     return rv
 
-j = requests.get('http://pheweb.sph.umich.edu:5001/api/manhattan/pheno/Blasts.json').json()
-f = open('input_files/snowstorm.txt', 'w')
-writer = csv.DictWriter(f, fieldnames='ref alt beg chrom maf pval'.split(), delimiter='\t', lineterminator='\n')
-writer.writeheader()
-for i, v in enumerate(j['unbinned_variants']):
-    if v['maf'] > 0.04:
-        writer.writerow(dict(
-            chrom=v['chrom'],
-            beg=v['pos'],
-            ref=v['ref'],
-            alt=v['alt'],
-            maf=format_float(random.random()/2),
-            pval=format_float(10 ** (-20*random.random())) if random.random() > 0.0001 else 0,
-        ))
+class TSVWriter:
+    def __init__(self, fname):
+        self.fname = fname
+    def __enter__(self):
+        self.f = open(self.fname, 'w')
+        return self
+    def writerow(self, dct):
+        if not hasattr(self, 'writer'):
+            self.writer = csv.DictWriter(self.f, fieldnames=list(dct.keys()), delimiter='\t', lineterminator='\n')
+            self.writer.writeheader()
+        self.writer.writerow(dct)
+    def __exit__(self, *args):
+        self.f.close()
+
+variants = requests.get('http://pheweb.sph.umich.edu/api/manhattan/pheno/601.json').json()['unbinned_variants']
+variants.append(dict(chrom='1', pos=869334, ref='G', alt='A'))
+chroms = [str(i) for i in range(1,22+1)] + ['X']
+variants = sorted(variants, key=lambda v: (chroms.index(v['chrom']), v['pos']))
+
+def make_pheno(pheno_name, use_maf, use_af, use_ac, use_ns):
+
+    ns = random.randrange(100, 10_000)
+
+    with TSVWriter(f'input_files/assoc-files/{pheno_name}.txt') as writer:
+
+        for v in variants:
+            if v['chrom'] == '1' and v['pos'] == 869334 or random.random() < 100 / len(variants):
+
+                d = dict(
+                    chrom=v['chrom'],
+                    pos=v['pos'],
+                    ref=v['ref'],
+                    alt=v['alt'],
+                    pval=format_float(random.random()),
+                )
+                ac = random.randrange(0,ns+1) # allow MAF=0 b/c I'm sure somebody will.
+                af = ac / ns
+                if use_maf: d['maf'] = format_float(af if af < 0.5 else 1-af)
+                if use_af: d['af'] = format_float(af)
+                if use_ac: d['ac'] = ac
+                if use_ns: d['ns'] = ns
+
+                writer.writerow(d)
+
+for x in itertools.product(*[(True, False)]*4):
+    args = dict(zip('af maf ac ns'.split(), x))
+    name = 'has-fields-' + '-'.join(k for k in sorted(args) if args[k])
+    make_pheno(name, *x)
