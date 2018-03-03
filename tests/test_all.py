676b43bef38a112708359c0e832d18f990063bed
Peter VandeHaar <pjvandehaar@gmail.com>
2018-03-03 17:08:19 -0500

    Make tests compat with python<3.6

--- tests/test_all.py
+++ tests/test_all.py
@@ -18,21 +18,21 @@ from pheweb.command_line import run as cl_run
 
 def test_all(tmpdir, capsys):
     p = tmpdir.realpath()
-    with capsys.disabled(): print(f'\n{p}')
+    with capsys.disabled(): print('\n{}'.format(p))
     indir = os.path.join(os.path.dirname(__file__), 'input_files/')
 
-    conf = ['conf', f'data_dir="{p}"', f'cache="{indir}/fake-cache"']
+    conf = ['conf', 'data_dir="{}"'.format(p), 'cache="{}/fake-cache"'.format(p)]
 
     assert pheweb.version.version.count('.') == 2
     cl_run(conf+['-h'])
-    cl_run(conf+['phenolist', 'glob', '--simple-phenocode', f'{indir}/assoc-files/*'])
+    cl_run(conf+['phenolist', 'glob', '--simple-phenocode', '{}/assoc-files/*'.format(indir)])
     cl_run(conf+['phenolist', 'unique-phenocode'])
     cl_run(conf+['phenolist', 'read-info-from-association-files'])
     cl_run(conf+['phenolist', 'filter-phenotypes', '--minimum-num-cases', '20', '--minimum-num-controls', '20', '--minimum-num-samples', '20'])
     cl_run(conf+['phenolist', 'hide-small-numbers-of-samples', '--minimum-visible-number', '50'])
     cl_run(conf+['phenolist', 'hide-small-numbers-of-samples', '--minimum-visible-number', '50'])
-    cl_run(conf+['phenolist', 'import-phenolist', '-f', f'{p}/pheno-list-categories.json', f'{indir}/categories.csv'])
-    cl_run(conf+['phenolist', 'merge-in-info', f'{p}/pheno-list-categories.json'])
+    cl_run(conf+['phenolist', 'import-phenolist', '-f', '{}/pheno-list-categories.json'.format(p), '{}/categories.csv'.format(indir)])
+    cl_run(conf+['phenolist', 'merge-in-info', '{}/pheno-list-categories.json'.format(p)])
     cl_run(conf+['phenolist', 'verify', '--required-columns', 'category'])
     cl_run(conf+['process'])
     cl_run(conf+['top-loci'])
