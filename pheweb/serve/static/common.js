d39e4826623def03d2ff1771eb6cdd7eebdc6eeb
Mitja Kurki <mitja.kurki@gmail.com>
2018-04-27 14:12:02 -0400

    download data from all pages for all variants that pass filtering

--- pheweb/serve/static/common.js
+++ pheweb/serve/static/common.js
@@ -85,57 +85,21 @@ function pValueToReadable(p) {
 
 
 function exportTableToCSV($table, filename) {
-    console.log("exporting")
-    var $headers = $table.find('tr:has(th)')
-        ,$rows = $table.find('tr:has(td)')
-        // Temporary delimiter characters unlikely to be typed by keyboard
-        // This is to avoid accidentally splitting the actual contents
-        ,tmpColDelim = String.fromCharCode(11) // vertical tab character
-        ,tmpRowDelim = String.fromCharCode(0) // null character
-        // actual delimiter characters for CSV format
-        ,colDelim = '","'
-        ,rowDelim = '"\r\n"';
-        // Grab text from table into CSV formatted string
-        var csv = '"';
-        csv += formatRows($headers.map(grabRow));
-        csv += rowDelim;
-        csv += formatRows($rows.map(grabRow)) + '"';
-        // Data URI
-        //var csvData = 'data:application/csv;charset=utf-8,' + encodeURIComponent(csv);
-        var createObjectURL = (window.URL || window.webkitURL || {}).createObjectURL || function(){};
-        var csvFile = new Blob([csv], {type: "text/csv"});
-        console.log(this)
-        var csvData = createObjectURL(csvFile)
+    var sTableData = $table.data('st').getData()
+    var colDelim = ','
+    var rowDelim = '\r\n'
+    var vals = sTableData[1]
+    var csv = Object.keys(sTableData[0]).join(colDelim)
+    csv+=rowDelim
+    csv+=sTableData.map( function( row ) { return Object.values(row).join(colDelim) }).join(rowDelim)
+    var createObjectURL = (window.URL || window.webkitURL || {}).createObjectURL || function(){}
+    var csvFile = new Blob([csv], {type: "text/csv"});
+    console.log(this)
+    var csvData = createObjectURL(csvFile)
     $(this)
         .attr({
         'download': filename
         ,'href':csvData
             //,'target' : '_blank' //if you want it to open in a new window
     });
-
-    //------------------------------------------------------------
-    // Helper Functions
-    //------------------------------------------------------------
-    // Format the output so it has the appropriate delimiters
-    function formatRows(rows){
-        return rows.get().join(tmpRowDelim)
-            .split(tmpRowDelim).join(rowDelim)
-            .split(tmpColDelim).join(colDelim);
-    }
-    // Grab and format a row from the table
-    function grabRow(i,row){
-
-        var $row = $(row);
-        //for some reason $cols = $row.find('td') || $row.find('th') won't work...
-        var $cols = $row.find('td');
-        if(!$cols.length) $cols = $row.find('th');
-        return $cols.map(grabCol)
-                    .get().join(tmpColDelim);
-    }
-    // Grab and format a column from the table
-    function grabCol(j,col){
-        var $col = $(col),
-            $text = $col.text();
-        return $text.replace('"', '""').replace(/\s+/g," "); // escape double quotes
-    }
 }
