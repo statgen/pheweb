a667cd49ce747c460e7415f342e7a545c58da1a3
Mitja Kurki <mitja.kurki@gmail.com>
2018-05-10 17:07:12 -0400

    Added configuration file

---
+++ config.py
@@ -0,0 +1,6 @@
+data_dir="/Users/mitja/projects/finngen/pheweb/remotedata/pheweb/pheweb"
+
+database_conf = ({ "variant":
+                    {"elastic": { "host":"35.187.119.225","port":9200, "variant_index":"finngen_r1_variant_annotation"}  }
+                 }
+                )
