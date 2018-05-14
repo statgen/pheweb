e9d43096b4e607d6088835a42bef11bfe2483ae4
Mitja Kurki <mitja.kurki@gmail.com>
2018-05-14 12:10:41 -0400

    Added LD from 1kg Finns. Switched UKBB and GWAS catalog to different symbols

--- pheweb/serve/static/gene_locusview.js
+++ pheweb/serve/static/gene_locusview.js
@@ -1,185 +1,7 @@
 'use strict';
 
-
-
-LocusZoom.Data.GWASCatSource = LocusZoom.Data.Source.extend(function(init) {
-    this.parseInit(init);
-}, "GWASCatSourceLZ");
-
-
-LocusZoom.Data.GWASCatSource.prototype.getURL = function(state, chain, fields) {
-    return this.url + "results/?format=objects&filter=id in " + this.params.id   +
-        " and chrom eq  '" + state.chr + "'" +
-        " and pos ge " + state.start +
-        " and pos le " + state.end;
-};
-
-
-LocusZoom.Data.GWASCatSource.prototype.parseResponse = function(resp, chain, fields, outnames, trans) {
-    var res = JSON.parse(resp)
-
-    console.log(fields)
-    console.log(outnames)
-
-    console.log(LocusZoom.Data.GWASCatSource.prototype)
-    if( res.data.length==0) {
-                // gotta have mock variant in correct format so LD search does not internal server arror
-        var dat = outnames.reduce(  function(acc, curr, i) { acc[curr]="0:0_a/t"; return acc }, {} )
-
-        return {header: chain.header, body:[dat] };
-    } else {
-        return LocusZoom.Data.Source.prototype.parseResponse.call(this,resp, chain, fields, outnames, trans);
-    }
-
-}
-
-
-LocusZoom.Data.ClinvarDataSource = LocusZoom.Data.Source.extend(function(init) {
-    this.parseInit(init);
-}, "ClinvarDataSourceLZ");
-
-
-LocusZoom.Data.ClinvarDataSource.prototype.getURL = function(state, chain, fields) {
-    return this.url
-};
-
-
-LocusZoom.Data.ClinvarDataSource.prototype.fetchRequest = function(state, chain, fields) {
-
-    var url = this.getURL(state, chain, fields);
-
-    var headers = {
-        "Content-Type": "application/x-www-form-urlencoded"
-    };
-    var requrl = url + "esearch.fcgi?db=clinvar&retmode=json&term=" + state.chr + "[chr]" + state.start + ":" + state.end + '[chrpos]%22clinsig%20pathogenic%22[Properties]&retmax=500'
-    return LocusZoom.createCORSPromise("GET", requrl).then(function( resp) {
-
-        var data = JSON.parse(resp);
-
-        if(data.esearchresult.count==0) {
-            var res = Q.defer()
-            console.log(state)
-            res.resolve( '{ "noresults":"","pos":' + state.start + ' }'  )
-            return res.promise
-        }
-
-        if (data.esearchresult.idlist != null) {
-            var requrl = url + "esummary.fcgi?db=clinvar&retmode=json&id=" + data.esearchresult.idlist.join(",")
-            return LocusZoom.createCORSPromise("GET", requrl)
-        } else {
-            var res = Q.defer()
-            console.log( "Failed to query clinvar" + JSON.stringify(data, null, 4 ) )
-            res.reject("Failed to query clinvar" + JSON.stringify(data, null, 4 ))
-            return res
-        }
-    }
-    );
-};
-
-LocusZoom.Data.ClinvarDataSource.prototype.parseResponse = function(resp, chain, fields, outnames, trans) {
-
-    var data = JSON.parse(resp)
-
-    if( data.noresults != null) {
-        // locuszoom does not show even axis titles if there are no data visible.
-        // make a mock element with id-1 which is set to invisible in the layout
-        var dat = fields.reduce(  function(acc, curr, i) { acc[curr]=-1; return acc }, {} )
-        return {header: chain.header, body:[dat] };
-    }
-
-    if (data.result==null) {
-            throw "error while processing clinvar:" +  data.esummaryresult
-    }
-    var respData = []
-    Object.entries(data.result).filter(function(x) {return x[0]!="uids"} ).forEach(function(val){
-
-        val = val[1]
-        var loc = val.variation_set[0].variation_loc.filter(function(x)  {return x.assembly_name=="GRCh38"} )[0]
-
-        var object= {}
-        object.start = loc.start;
-        object.stop = loc.stop;
-        object.ref = loc.ref;
-        object.alt = loc.alt;
-        object.chr = loc.chr
-        object.varName = val.variation_set[0].variation_name;
-        object.clinical_sig = val.clinical_significance.description;
-        object.trait = val.trait_set.map( function(x) { return x.trait_name } ).join(":")
-        object.y= 5
-        object.id = val.uid;
-
-        respData.push( object )
-    });
-    return {header: chain.header, body: respData};
-};
-
-
-LocusZoom.Data.GeneConstraintSource.prototype.fetchRequest = function(state, chain, fields) {
-    var geneids = [];
-    chain.body.forEach(function(gene){
-        var gene_id = gene.gene_id;
-        if (gene_id.indexOf(".")){
-            gene_id = gene_id.substr(0, gene_id.indexOf("."));
-        }
-        geneids.push(gene_id);
-    });
-    var url = this.getURL(state, chain, fields);
-    var body = "geneids=" + encodeURIComponent(JSON.stringify(geneids));
-    var headers = {
-        "Content-Type": "application/x-www-form-urlencoded"
-    };
-    return LocusZoom.createCORSPromise("POST", this.url, body, headers);
-
-};
-
-LocusZoom.Data.AssociationSource.prototype.parseArraysToObjects = function(x, fields, outnames, trans) {
-    // This overrides the default to keep all fields in `x` (the response)
-    // If <https://github.com/statgen/locuszoom/pull/102> gets accepted, it won't be necessary.
-
-    //intended for an object of arrays
-    //{"id":[1,2], "val":[5,10]}
-    if (Object.keys(x).length === 0) {
-        throw "The association source sent back no data for this region.";
-    }
-    var records = [];
-    fields.forEach(function(f, i) {
-          if (!(f in x)) {throw "field " + f + " not found in response for " + outnames[i];}
-    });
-    var x_keys = Object.keys(x);
-    var N = x[x_keys[0]].length; // NOTE: this was [1] before, why?
-    x_keys.forEach(function(key) {
-        if (x[key].length !== N) {
-            throw "the response column " + key + " had " + x[key].length.toString() +
-                " elements but " + x_keys[0] + " had " + N.toString();
-        }
-    });
-    var nonfield_keys = x_keys.filter(function(key) {
-        return fields.indexOf(key) === -1;
-    });
-    for(var i = 0; i < N; i++) {
-        var record = {};
-        for(var j=0; j<fields.length; j++) {
-            var val = x[fields[j]][i];
-            if (trans && trans[j]) {
-                val = trans[j](val);
-            }
-            record[outnames[j]] = val;
-        }
-        for(var j=0; j<nonfield_keys.length; j++) {
-            record[nonfield_keys[j]] = x[nonfield_keys[j]][i];
-        }
-        records.push(record);
-    }
-    return records;
-};
-
-LocusZoom.TransformationFunctions.set("percent", function(x) {
-    if (x === 1) { return "100%"; }
-    var x = (x*100).toPrecision(2);
-    if (x.indexOf('.') !== -1) { x = x.replace(/0+$/, ''); }
-    if (x.endsWith('.')) { x = x.substr(0, x.length-1); }
-    return x + '%';
-});
+// DEPENDENCIES: This js depends on custom_locuszoom.js, which need to be included first in html files. We are moving to webpack to take care of the dependencies and this documentation is
+// an interim reminder
 
 (function() {
     // Define LocusZoom Data Sources object
@@ -189,10 +11,12 @@ LocusZoom.TransformationFunctions.set("percent", function(x) {
     data_sources.add("gwas_cat", new LocusZoom.Data.GWASCatSource({url: remoteBase + "annotation/gwascatalog/", params: { id:[1,4] ,pvalue_field: "log_pvalue" }}));
     data_sources.add("base", ["AssociationLZ", localBase]);
     data_sources.add("gene", ["GeneLZ", {url:remoteBase + "annotation/genes/", params:{source:1}}])
-    data_sources.add("ld", ["LDLZ", {url: remoteBase + "pair/LD/", params: { source:1 ,pvalue_field: "pvalue|neglog10_or_100" }}]);
     data_sources.add("constraint", ["GeneConstraintLZ", { url: "http://exac.broadinstitute.org/api/constraint" }])
     // clinvar needs to be added after gene because genes within locuszoom data chain are used for fetching
     data_sources.add("clinvar", new LocusZoom.Data.ClinvarDataSource({url: "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/", params: { id:[1,4] ,pvalue_field: "log_pvalue" }}));
+
+    data_sources.add("ld", new LocusZoom.Data.FG_LDDataSource({url: "https://rest.ensembl.org/ld/homo_sapiens/", params: { id:[1,4] ,pvalue_field: "pvalue", "var_id_field":"rsid" }}));
+
 
     // https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=clinvarid=65533retmode=json
 
@@ -474,7 +298,7 @@ LocusZoom.TransformationFunctions.set("percent", function(x) {
                     "class": "lz-data_layer-scatter"
                 }],
 
-                fields: ["id", "chr", "position", "ref", "alt", "pvalue", "pvalue|neglog10_or_100", "ld:state", "ld:isrefvar"],
+                fields: ["id", "chr","rsid","position", "ref", "alt", "pvalue", "pvalue|neglog10_or_100", "ld:state", "ld:isrefvar"],
                 // ldrefvar can only be chosen if "pvalue|neglog10_or_100" is present.  I forget why.
                 id_field: "id",
                 behaviors: {
@@ -579,8 +403,7 @@ LocusZoom.TransformationFunctions.set("percent", function(x) {
             },
             "data_layers": [ {
                 "namespace": {
-                    "gwas_cat":"gwas_cat",
-                    "ld": "ld"
+                    "gwas_cat":"gwas_cat"
                 },
                 "id": "gwas_cat:id",
                 "type": "scatter",
@@ -589,8 +412,8 @@ LocusZoom.TransformationFunctions.set("percent", function(x) {
                     "field": "gwas_cat:study",
                     "parameters": {
                         "field_value": "UKBB",
-                        "then": "diamond",
-                        "else":"circle"
+                        "then": "circle",
+                        "else":"diamond"
                     }
                 },
                 "color": {
@@ -604,20 +427,20 @@ LocusZoom.TransformationFunctions.set("percent", function(x) {
                 },
                 fill_opacity: 0.7,
                 "legend": [{
-                    "shape": "diamond",
+                    "shape": "circle",
                     "color": "#9632b8",
                     "size": 40,
                     "label": "UKBB",
                     "class": "lz-data_layer-scatter"
                 }, {
-                    "shape": "circle",
+                    "shape": "diamond",
                     "color": "#d43f3a",
                     "size": 40,
                     "label": "GWAS catalog",
                     "class": "lz-data_layer-scatter"
                 },],
 
-                fields: ["gwas_cat:id", "gwas_cat:or_beta","gwas_cat:pmid","gwas_cat:variant","gwas_cat:chrom", "gwas_cat:risk_allele", "gwas_cat:risk_frq","gwas_cat:pos", "gwas_cat:ref", "gwas_cat:alt","gwas_cat:trait","gwas_cat:study", "gwas_cat:log_pvalue", "ld:state", "ld:isrefvar"],
+                fields: ["gwas_cat:id", "gwas_cat:or_beta","gwas_cat:pmid","gwas_cat:variant","gwas_cat:chrom", "gwas_cat:risk_allele", "gwas_cat:risk_frq","gwas_cat:pos", "gwas_cat:ref", "gwas_cat:alt","gwas_cat:trait","gwas_cat:study", "gwas_cat:log_pvalue"],
 
                 id_field: "gwas_cat:variant",
                 behaviors: {
