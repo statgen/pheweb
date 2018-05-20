4469ca8a810060c0e462762e9da5d55a92d2c40e
Mitja Kurki <mitja.kurki@gmail.com>
2018-05-19 20:00:38 -0400

    del con

--- config.py
+++
@@ -1,7 +0,0 @@
-data_dir="/mnt/data-disk/pheweb"
-
-database_conf = ({ "annotation":
-                    {"elastic": { "host":"35.187.119.225","port":9200, "variant_index":"finngen_r1_variant_annotation"}  }
-                 },
-                 { "result": { "tabix": { 'const_arguments': [("phenos","PHEWEB_PHENOS"), ("matrix_path","MATRIX_PATH")] } } }
-                )
