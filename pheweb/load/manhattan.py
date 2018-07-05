21428d64b6e87c20bb0be6b35cd0b35c60f820df
Peter VandeHaar <pjvandehaar@gmail.com>
2018-07-05 06:33:21 -0400

    Widen qval bins in manhattan plot as plot gets taller (up until qval=40)

--- pheweb/load/manhattan.py
+++ pheweb/load/manhattan.py
@@ -5,12 +5,22 @@ This script creates json files which can be used to render Manhattan plots.
 
 # NOTE: `qval` means `-log10(pvalue)`
 
+# TODO: optimize binning for fold@20 view.
+#       - if we knew the max_qval before we started (eg, by running qq first), it would be very easy.
+#       - at present, we set qval bin size well for the [0-40] range but not for variants above that.
+
 from ..utils import chrom_order
 from ..conf_utils import conf
 from ..file_utils import VariantFileReader, write_json, common_filepaths
 from .load_utils import MaxPriorityQueue, parallelize_per_pheno
 
 import math
+
+BIN_LENGTH = int(3e6)
+
+QVAL_BIN_BASE_SIZE = 0.05 # Use 0.05, 0.1, 0.15, etc
+QVAL_BIN_DIGITS = 2 # Then round to this many digits
+QVAL_MIN_NUM_BINS = 200 # Try to keep at least this many bins for [0 - min(40,max_qval)]
 
 
 def run(argv):
@@ -23,17 +33,9 @@ def run(argv):
 
 
 def make_json_file(pheno):
-    BIN_LENGTH = int(3e6)
-    QVAL_BIN_SIZE = 0.05 # Use 0.05, 0.1, 0.15, etc
-    QVAL_BIN_DIGITS = 2 # Then round to this many digits
 
     with VariantFileReader(common_filepaths['pheno'](pheno['phenocode'])) as variants:
-        variant_bins, unbinned_variants = bin_variants(
-            variants,
-            BIN_LENGTH,
-            QVAL_BIN_SIZE,
-            QVAL_BIN_DIGITS
-        )
+        variant_bins, unbinned_variants = bin_variants(variants)
     label_peaks(unbinned_variants)
     rv = {
         'variant_bins': variant_bins,
@@ -43,8 +45,8 @@ def make_json_file(pheno):
     write_json(filepath=common_filepaths['manhattan'](pheno['phenocode']), data=rv)
 
 
-def rounded_neglog10(pval, qval_bin_size, qval_bin_digits):
-    return round(-math.log10(pval) // qval_bin_size * qval_bin_size, qval_bin_digits)
+def rounded_neglog10(pval, qval_bin_size):
+    return round(-math.log10(pval) // qval_bin_size * qval_bin_size, QVAL_BIN_DIGITS)
 
 
 def get_pvals_and_pval_extents(pvals, qval_bin_size):
@@ -65,28 +67,37 @@ def get_pvals_and_pval_extents(pvals, qval_bin_size):
     return (rv_pvals, rv_pval_extents)
 
 
-# TODO: convert bins from {(chrom, pos): []} to {chrom:{pos:[]}} to make label_peaks easier?
-def bin_variants(variant_iterator, bin_length, qval_bin_size, qval_bin_digits):
+# TODO: convert bins from {(chrom, pos): []} to {chrom:{pos:[]}} to eliminate `chrom_n_bins` ?
+def bin_variants(variant_iterator):
     bins = {}
     unbinned_variant_pq = MaxPriorityQueue()
     chrom_n_bins = {}
+    qval_bin_size = QVAL_BIN_BASE_SIZE
+    min_pval = 0.999 # almost 1, but avoids divide-by-zero errors
 
     def bin_variant(variant):
         chrom_key = chrom_order[variant['chrom']]
-        pos_bin = variant['pos'] // bin_length
+        pos_bin = variant['pos'] // BIN_LENGTH
         chrom_n_bins[chrom_key] = max(chrom_n_bins.get(chrom_key,0), pos_bin)
         if (chrom_key, pos_bin) in bins:
             bin = bins[(chrom_key, pos_bin)]
 
         else:
-            bin = {"chrom": variant['chrom'],
-                   "startpos": pos_bin * bin_length,
-                   "qvals": set()}
-            bins[(chrom_key, pos_bin)] = bin
-        bin["qvals"].add(rounded_neglog10(variant['pval'], qval_bin_size, qval_bin_digits))
+            bin = bins[(chrom_key, pos_bin)] = {"chrom": variant['chrom'],
+                                                "startpos": pos_bin * BIN_LENGTH,
+                                                "qvals": set()}
+        bin["qvals"].add(rounded_neglog10(variant['pval'], qval_bin_size))
 
     # put most-significant variants into the priorityqueue and bin the rest
     for variant in variant_iterator:
+        if variant['pval'] < min_pval and variant['pval'] != 0:
+            min_pval = variant['pval']
+            # if this new min_pval gives us 2x+ as many qval_bins as we need,
+            # then double the qval_bin_size and re-bin all qvals we currently have
+            if min(40, -math.log10(min_pval)) / qval_bin_size >= QVAL_MIN_NUM_BINS * 2:
+                qval_bin_size *= 2
+                for bin in bins:
+                    bin['qvals'] = {rounded_neglog10(qval, qval_bin_size) for qval in bin['qvals']}
         unbinned_variant_pq.add(variant, variant['pval'])
         if len(unbinned_variant_pq) > conf.manhattan_num_unbinned:
             old = unbinned_variant_pq.pop()
@@ -101,7 +112,7 @@ def bin_variants(variant_iterator, bin_length, qval_bin_size, qval_bin_digits):
             b = bins.get((chrom_key, pos_key), None)
             if b and len(b['qvals']) != 0:
                 b['qvals'], b['qval_extents'] = get_pvals_and_pval_extents(b['qvals'], qval_bin_size)
-                b['pos'] = int(b['startpos'] + bin_length/2)
+                b['pos'] = int(b['startpos'] + BIN_LENGTH/2)
                 del b['startpos']
                 binned_variants.append(b)
 
