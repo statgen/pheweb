c1d3c18e37c8bc26d84427a7f3863c7a5a0e8754
Peter VandeHaar <pjvandehaar@gmail.com>
2018-12-07 01:42:58 -0500

    Build `./setup.py publish`

--- setup.py
+++ setup.py
@@ -26,6 +26,58 @@ if sys.version_info[:2] == (3,7):
 
 version = imp.load_source('pheweb.version', os.path.join('pheweb', 'version.py')).version
 
+
+if sys.argv[-1] in ['publish', 'pub']:
+    import subprocess, json
+    from pathlib import Path
+    from urllib.request import urlopen
+    # TODO: use `class UploadCommand(setuptools.Command)` from <https://github.com/kennethreitz/setup.py/blob/master/setup.py#L49>
+
+    git_workdir_returncode = subprocess.run('git diff-files --quiet'.split()).returncode
+    assert git_workdir_returncode in [0,1]
+    if git_workdir_returncode == 1:
+        print('=> git workdir has changes')
+        print('=> please either revert or stage them')
+        sys.exit(1)
+
+    pypi_url = 'https://pypi.python.org/pypi/PheWeb/json'
+    latest_version = json.loads(urlopen(pypi_url).read())['info']['version']
+    # Note: it takes pypi a minute to update the API, so this can be wrong.
+    if latest_version == version:
+        new_version_parts = version.split('.')
+        new_version_parts[2] = str(1+int(new_version_parts[2]))
+        new_version = '.'.join(new_version_parts)
+        print(f'=> autoincrementing version {version} -> {new_version}')
+        Path('pheweb/version.py').write_text(f"version = '{new_version}'\n")
+        version = new_version
+        subprocess.run(['git','stage','pheweb/version.py'])
+
+    git_index_returncode = subprocess.run('git diff-index --quiet --cached HEAD'.split()).returncode
+    assert git_index_returncode in [0,1]
+    if git_index_returncode == 1:
+        print('=> git index has changes')
+        subprocess.run(['git','commit','-m',version])
+
+    if not Path('~/.pypirc').expanduser().exists():
+        print('=> warning: you need a ~/.pypirc')
+
+    if Path('dist').exists() and list(Path('dist').iterdir()):
+        setuppy = Path('setup.py').absolute()
+        assert setuppy.is_file() and 'pheweb' in setuppy.read_text()
+        for child in Path('dist').absolute().iterdir():
+            assert child.name.startswith('PheWeb-'), child
+            print('=> unlinking', child)
+            child.unlink()
+
+    subprocess.run('python3 setup.py sdist bdist_wheel'.split(), check=True)
+    subprocess.run('twine upload dist/*'.split(), check=True)
+
+    if git_index_returncode == 1:
+        print('=> Now do `git push`.')
+    sys.exit(0)
+
+
+
 setup(
     name='PheWeb',
     version=version,
