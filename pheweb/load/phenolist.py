8821cabf6a5059a21bbec3e35b164aba96a4c6b6
Juha Karjalainen <jkarjala@broadinstitute.org>
2020-03-13 18:44:48 +0000

    phenolist r5

--- pheweb/load/phenolist.py
+++ pheweb/load/phenolist.py
@@ -2,7 +2,7 @@
 
 # use this to add metadata to the pheno-list.json file: endpoint names, categories, case/control n, gc lambda, num gw sig hits
 # TODO make part of the import wdl, not using the cov_pheno file but precalculating Ns so can be done without red data
-# phenolist.py pheno-list.json.orig ../r3_1/pheno-list.json TAGLIST_DF4.txt finngen_R4_endpoint_definition.txt R4_COV_PHENO_V1.txt.gz ./generated-by-pheweb | python -m json.tool > pheno-list.json
+# python3 /mnt/nfs/juha/pheweb/pheweb/load/phenolist.py /mnt/nfs/pheweb/r5/pheno-list.json.orig /mnt/nfs/pheweb/r4/pheno-list.json TAGLIST_DF5.txt FINNGEN_ENDPOINTS_DF5_V2_2020-02-11.names_tagged_ordered.txt Endpoints_Controls_FINNGEN_ENDPOINTS_DF5_V2_2020-02-11.utf8.exnames.txt R5_COV_PHENO_V1.txt.gz /mnt/nfs/pheweb/r5/generated-by-pheweb > /mnt/nfs/pheweb/r5/pheno-list.json
 
 import sys
 import json
@@ -20,58 +20,40 @@ def run():
     code2pheno = {pheno['phenocode']:pheno for pheno in phenolist}
     code2pheno_prev = {pheno['phenocode']:pheno for pheno in phenolist_prev}
     tag2name = {}
-    tag2codes = od()
-    
+    code2tag = {}
+    tag2index = {}
+
     with open(sys.argv[3]) as f:
         f.readline()
         for line in f:
             s = line.strip().split('\t')
-            tag2codes[s[0].strip()] = {}
             tag2name[s[0].strip()] = s[len(s)-1].strip()
 
     with open(sys.argv[4]) as f:
+        f.readline()
+        for line in f:
+            s = line.strip().split('\t')
+            code2tag[s[2]] = s[0]
+            if s[0] not in tag2index:
+                tag2index[s[0]] = len(tag2index)
+
+    with open(sys.argv[5]) as f:
         h = {h.strip():i for i,h in enumerate(f.readline().strip().split('\t'))}
         f.readline()
         for line in f:
             s = [s.strip() for s in line.strip().split('\t')]
             code = s[h['NAME']].strip()
-            if code not in code2pheno and code + '_EXMORE' in code2pheno:
-                code = code + '_EXMORE'
-            if code not in code2pheno and code + '_EXALLC' in code2pheno:
-                code = code + '_EXALLC'
-            if code in code2pheno:# or code + '_EXMORE' in code2pheno or code + '_EXALLC' in code2pheno:
+            if code in code2pheno:
                 code2pheno[code]['phenostring'] = s[h['LONGNAME']].strip()
                 code2pheno[code]['num_cases'] = 0
                 code2pheno[code]['num_controls'] = 0
-                tags_this = [t.strip().replace('"', '') for t in s[h['TAGS']].split(',')]
-                min = 1000
-                min_tag = None
-                for tag in tags_this:
-                    i = list(tag2codes.keys()).index(tag)
-                    if i < min:
-                        min = i
-                        min_tag = tag
-                category = tag2name[min_tag]
-                code2pheno[code]['category'] = category
-                tag2codes[min_tag][code] = True
-                ex = ['_EXMORE', '_EXALLC']
-                for e in ex:
-                    if (code + e) in code2pheno:
-                        code2pheno[code + e]['phenostring'] = s[h['LONGNAME']].strip() + ' (more controls excluded)' if e == '_EXMORE' else s[h['LONGNAME']].strip() + ' (other cancers excluded from controls)'
-                        code2pheno[code + e]['num_cases'] = 0
-                        code2pheno[code + e]['num_controls'] = 0
-                        code2pheno[code + e]['category'] = category
-                        tag2codes[min_tag][code + e] = True
+                code2pheno[code]['category'] = tag2name[code2tag[code]]
+                code2pheno[code]['category_index'] = tag2index[code2tag[code]]
 
-    for tag in tag2codes:
-        for code in tag2codes[tag]:
-            if len(tag2codes[tag]) < 3:
-                code2pheno[code]['category'] = 'Other'
-    
-    with gzip.open(sys.argv[5], 'rt') as f:
+    with gzip.open(sys.argv[6], 'rt') as f:
         header_index = [(i, h) for i,h in enumerate(f.readline().strip().split('\t')) if h in code2pheno]
         for line in f:
-            s = line.split('\t')
+            s = line.strip().split('\t')
             for i_h in header_index:
                 if s[i_h[0]] == '1':
                     code2pheno[i_h[1]]['num_cases'] = code2pheno[i_h[1]]['num_cases'] + 1
@@ -79,10 +61,10 @@ def run():
                     code2pheno[i_h[1]]['num_controls'] = code2pheno[i_h[1]]['num_controls'] + 1
 
     for pheno in phenolist:
-        with open(sys.argv[6] + '/qq/' + pheno['phenocode'] + '.json') as f:
+        with open(sys.argv[7] + '/qq/' + pheno['phenocode'] + '.json') as f:
             qq = json.load(f)
             pheno['gc_lambda'] = qq['overall']['gc_lambda']
-        with open(sys.argv[6] + '/manhattan/' + pheno['phenocode'] + '.json') as f:
+        with open(sys.argv[7] + '/manhattan/' + pheno['phenocode'] + '.json') as f:
             manha = json.load(f)
             pheno['num_gw_significant'] = len([v for v in manha['unbinned_variants'] if 'peak' in v and v['peak'] == True and float(v['pval']) < 5e-8])
         if pheno['phenocode'] in code2pheno_prev:
