563563980a1202737340a8547f9524b5b19a4063
major seitan <majorseitan@gmail.com>
2020-07-09 09:56:07 +0000

    adding colocalization rest endpoints

---
+++ pheweb/serve/colocalization/app.py
@@ -0,0 +1,17 @@
+import typing
+import click
+from flask import Flask, send_from_directory, request, g
+import logging
+import json
+import os
+from .cli import data_cli
+from .model_db import ColocalizationDB
+import atexit
+import sys
+#sys.path.append(".")
+
+db_url = os.getenv('SQLALCHEMY_DATABASE_URI', 'sqlite:////tmp/tmp.db')
+
+app = Flask(__name__, static_folder='static')
+
+app.cli.add_command(data_cli)
