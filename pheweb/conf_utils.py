ee730cf787f07861a8591180d3c5c59895757bb2
Peter VandeHaar <pjvandehaar@gmail.com>
2020-07-16 15:22:40 -0700

    Fix conf.field_aliases vs conf.aliases mixup

--- pheweb/conf_utils.py
+++ pheweb/conf_utils.py
@@ -355,7 +355,7 @@ def _ensure_conf():
 
     if 'aliases' in conf: conf['field_aliases'] = conf['aliases']
     if 'field_aliases' in conf:
-        for alias, field in conf.aliases.items():
+        for alias, field in conf['field_aliases'].items():
             conf.parse.fields[field].setdefault('aliases', []).append(alias)
 
     if 'null_values' in conf:
