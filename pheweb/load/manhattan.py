9e49b81f077d25b837aea53225bd0a2c7595fbf2
Peter VandeHaar <pjvandehaar@gmail.com>
2020-11-06 12:07:13 -0500

    Hopefully fix error when binning pval=0 variants for manhattan plot

--- pheweb/load/manhattan.py
+++ pheweb/load/manhattan.py
@@ -123,7 +123,7 @@ class Binner:
         pos_bin_id = variant['pos'] // BIN_LENGTH
         if pos_bin_id not in self._bins[chrom_idx]:
             self._bins[chrom_idx][pos_bin_id] = {'chrom': variant['chrom'], 'startpos': pos_bin_id * BIN_LENGTH, 'qvals': set()}
-        qval = self._rounded(-math.log10(variant['pval']))
+        qval = math.inf if variant['pval'] == 0 else self._rounded(-math.log10(variant['pval']))
         self._bins[chrom_idx][pos_bin_id]["qvals"].add(qval)
 
     def get_result(self):
