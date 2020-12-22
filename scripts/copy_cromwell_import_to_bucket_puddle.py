f5e6dea7631e75fa78c0acf959c5753f703e2976
Mitja Kurki <mitja.kurki@gmail.com>
2020-12-22 14:49:16 -0500

    fixed error messages and the wrong var name

--- scripts/copy_cromwell_import_to_bucket_puddle.py
+++ scripts/copy_cromwell_import_to_bucket_puddle.py
@@ -7,7 +7,7 @@ import json
 import os
 import time
 
-basic_output_nodes =["pheweb_import.matrix.matrix", "pheweb_import.matrix.matrix_tbi",
+output_nodes =["pheweb_import.matrix.matrix", "pheweb_import.matrix.matrix_tbi",
     "pheweb_import.matrix.pheno_gene","pheweb_import.matrix.phenolist",
     "pheweb_import.matrix.top_hits_1k","pheweb_import.matrix.top_hits_json","pheweb_import.matrix.top_hits_tsv",
     "pheweb_import.pheno.manhattan","pheweb_import.pheno.pheno_gz","pheweb_import.pheno.pheno_tbi","pheweb_import.pheno.qq"]
@@ -17,10 +17,10 @@ annot_nodes = ["pheweb_import.annotation.bed", "pheweb_import.annotation.gene_tr
 
 def run():
     parser = argparse.ArgumentParser(description="Run x-way meta-analysis")
-    parser.add_argument('cromwell_hash', action='store', type=str, help='Configuration file ')
-    parser.add_argument('destination_bucket', action='store', type=str, help='Result file')
+    parser.add_argument('cromwell_hash', action='store', type=str, help='Cromwell hash ')
+    parser.add_argument('destination_bucket', action='store', type=str, help='Destination bucket to copy the files')
 
-    parser.add_argument('--skip_annotation', action='store', type=str, help='Result file')
+    parser.add_argument('--skip_annotation', action='store_true', help='Result file')
 
     parser.add_argument('--cromwell_url', default="localhost", action='store', type=str, help='Cromwell URL')
     parser.add_argument('--socks_proxy', default="localhost:5000", action='store', type=str, help='Cromwell URL')
@@ -42,7 +42,8 @@ def run():
 
     ret = json.loads( pr.stdout )
 
-    if "status " in ret and ret['status']=='fail' :
+    print(args.skip_annotation)
+    if "status" in ret and ret['status']=='fail' :
         raise Exception(f'Error requesting metadata. Cromwell message: {ret["message"]}')
 
     all_files = {}
