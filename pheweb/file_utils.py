8aea49e0ce39b900e189106c984ce0ba4762e87f
Peter VandeHaar <pjvandehaar@gmail.com>
2021-01-12 13:23:34 -0500

    Always write variant files as `gzip -2` (but don't change file extension for backwards compatibility)

--- pheweb/file_utils.py
+++ pheweb/file_utils.py
@@ -321,7 +321,8 @@ def VariantFileWriter(filepath, allow_extra_fields=False):
     part_file = get_tmp_path(filepath)
     make_basedir(filepath)
     with AtomicSaver(filepath, text_mode=True, part_file=part_file, overwrite_part=True, rm_part_on_exc=False) as f:
-        yield _vfw(f, allow_extra_fields, filepath)
+        with gzip.open(f, 'w', compresslevel=2) as f_gzip:
+            yield _vfw(f_gzip, allow_extra_fields, filepath)
 class _vfw:
     def __init__(self, f, allow_extra_fields, filepath):
         self._f = f
