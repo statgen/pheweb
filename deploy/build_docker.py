802dc4a6c7549bc398524db788c3310501d647ba
piotor87 <pietro.dellabriottaparolo@gmail.com>
2021-01-27 16:36:44 +0200

    changes

--- deploy/build_docker.py
+++ deploy/build_docker.py
@@ -12,22 +12,24 @@ if __name__ == "__main__":
     parser.add_argument("--image", type= str,
                         help="name of image",default = 'pheweb_annotation')
     parser.add_argument("--docker", type= str,
-                        help="name of image",default = 'Dockerfile_annotation')
+                        help="name of image",default = 'Dockerfile')
 
     parser.add_argument("--version", type= str,
                         help="version value, e.g.0.001",required = True)
+    parser.add_argument("--project", type= str,default = 'phewas-development')
+
     parser.add_argument("--push",action = 'store_true')
     parser.add_argument("--args",type = str,default = '')
     args = parser.parse_args()
 
     
-    basic_cmd = 'docker build -t eu.gcr.io/finngen-refinery-dev/' + args.image +':' +args.version
+    basic_cmd = f'docker build -t eu.gcr.io/{args.project}/' + args.image +':' +args.version
     cmd = basic_cmd + f' -f {args.docker}  .' + ' ' + args.args
     print(cmd)
     call(shlex.split(cmd))
 
     if args.push:
-        cmd = ' docker -- push eu.gcr.io/finngen-refinery-dev/' + args.image +':' + args.version
+        cmd = f' docker -- push eu.gcr.io/{args.project}/' + args.image +':' + args.version
         print(cmd)
         call(shlex.split(cmd))
 
