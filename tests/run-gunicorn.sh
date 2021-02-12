1e9e22e53c272d0786c2fa4eb74530521ce48f0c
Peter VandeHaar <pjvandehaar@gmail.com>
2021-02-12 13:37:29 -0500

    .

--- tests/run-gunicorn.sh
+++ tests/run-gunicorn.sh
@@ -13,9 +13,9 @@ ln -s -f "$script_dir/input_files/custom_templates" "$data_dir/"
 if ! [[ -f "$data_dir/pheno-list.json" ]]; then ln -s -f "$script_dir/pheno-list.json" "$data_dir/"; fi
 for f in "$script_dir"/generated-by-pheweb/*; do ln -s -f "$f" "$data_dir/generated-by-pheweb/"; done;
 
-echo "http://localhost:8000/"
+echo "http://localhost:5000/"
 
 cd "$data_dir"
-python3 "$(which gunicorn)" --error-logfile=- --access-logfile=- --access-logformat="%(s)s | %(L)ss | %(f)s | %(m)s %(U)s %(q)s" -w4 --reload --pythonpath "$script_dir/.." pheweb.serve.server:app
+python3 "$(which gunicorn)" --bind="localhost:5000" --error-logfile=- --access-logfile=- --access-logformat="%(s)s | %(L)ss | %(f)s | %(m)s %(U)s %(q)s" -w4 --reload --pythonpath "$script_dir/.." pheweb.serve.server:app
 
-#pheweb conf data_dir="$data_dir" serve --host='localhost' --port=8000 --num-workers=2
+#pheweb conf data_dir="$data_dir" serve --host='localhost' --port=5000 --num-workers=2
