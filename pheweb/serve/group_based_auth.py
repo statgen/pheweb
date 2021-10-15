608aa701a45357086cece1e8d412f3a40c135afe
major seitan <majorsetian@gmail.com>
2021-10-15 15:30:23 +0000

    feature: add support for multiple groups
    added support for a "GROUPS" attribute
    consisting of a list.

--- pheweb/serve/group_based_auth.py
+++ pheweb/serve/group_based_auth.py
@@ -7,25 +7,38 @@ from googleapiclient.discovery import build
 from google.oauth2 import service_account
 
 if conf["authentication"]:
-    group_name = conf.group_auth['GROUP']
-    service_account_file = conf.group_auth['SERVICE_ACCOUNT_FILE']
-    delegated_account = conf.group_auth['DELEGATED_ACCOUNT']
+    group_names = []
+    # get group names from either the "GROUP"
+    # attribute containing a single group name
+    # (to support legacy configurations) or get
+    # a list from "GROUPS"
+    if "GROUP" in conf.group_auth:
+        group_names.append(conf.group_auth["GROUP"])
+    elif "GROUPS":
+        group_names.extend(conf.group_auth["GROUPS"])
+
+    service_account_file = conf.group_auth["SERVICE_ACCOUNT_FILE"]
+    delegated_account = conf.group_auth["DELEGATED_ACCOUNT"]
 
     service_account_scopes = [
-        'https://www.googleapis.com/auth/admin.directory.group.readonly',
-        'https://www.googleapis.com/auth/admin.directory.user.readonly',
-        'https://www.googleapis.com/auth/admin.directory.group.member.readonly'
+        "https://www.googleapis.com/auth/admin.directory.group.readonly",
+        "https://www.googleapis.com/auth/admin.directory.user.readonly",
+        "https://www.googleapis.com/auth/admin.directory.group.member.readonly",
     ]
 
     # set credentials
-    creds = service_account.Credentials.from_service_account_file(service_account_file, scopes=service_account_scopes)
+    creds = service_account.Credentials.from_service_account_file(
+        service_account_file, scopes=service_account_scopes
+    )
     delegated_creds = creds.with_subject(delegated_account)
-    services = defaultdict( lambda: build('admin', 'directory_v1', credentials=delegated_creds) )
+    services = defaultdict(
+        lambda: build("admin", "directory_v1", credentials=delegated_creds)
+    )
 
-    whitelist = conf.login['whitelist'] if 'whitelist' in conf.login.keys() else []
+    whitelist = conf.login["whitelist"] if "whitelist" in conf.login.keys() else []
 
 else:
-    group_name = None
+    group_names = []
     service_account_file = None
     delegated_account = None
     service_account_scopes = None
@@ -34,42 +47,42 @@ else:
     services = None
     whitelist = None
 
-def get_all_members(group_name):
-    all = services[threading.get_ident()].members().list(groupKey=group_name).execute()
-    allmembers = all['members']
-    return allmembers
+
+def get_all_members(group_names):
+    members = []
+    for name in group_names:
+        all = services[threading.get_ident()].members().list(groupKey=name).execute()
+        memebers.extend(all["members"])
+    return members
 
 
 def get_member_status(username):
-    allmembers = get_all_members(group_name)
+    allmembers = get_all_members(group_names)
 
     for m in allmembers:
-        user = m['email']
+        user = m["email"]
         if "gserviceaccount" in user:  # service accounts are excluded
             continue
         if user == username:
-            return m['status']
-
+            return m["status"]
 
 
 def verify_membership(username):
 
     if username in whitelist:
         return True
-
     # auth service .hasMember will only work for accounts of the domain
-    if not username.endswith('@finngen.fi'):
+    elif not username.endswith("@finngen.fi"):
         return False
-
-    r = services[threading.get_ident()].members().hasMember(groupKey=group_name, memberKey=username).execute()
-
-    return r['isMember']
-
-    ## Members are grouped to one so don't check for individual member status
-    # status = get_member_status(username)
-    # if status == 'ACTIVE':
-    #     #print('\n\n' + username + ' is a member of ' + group_name + ' with status = ' + status)  # user is an ACTIVE member
-    #     return True
-    # else:
-    #     #print('\n\n' + username + ' is a member of ' + group_name + ' with status = ' + status)  # user is a SUSPENDED member
-    #     return False
+    else:
+        for name in group_names:
+            r = (
+                services[threading.get_ident()]
+                .members()
+                .hasMember(groupKey=name, memberKey=username)
+                .execute()
+            )
+            if r["isMember"] is True:
+                return True
+    # default to false
+    return False
