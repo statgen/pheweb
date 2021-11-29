dce01829dbcc3e6cc0676819b48657f6d7a0c735
major seitan <majorsetian@gmail.com>
2021-11-29 22:51:13 +0000

    wip:add unit test   added prettier   fix broken tests   improve test coverage

--- pheweb/serve/react/js/components/NotFound/NotFound.test.tsx
+++ pheweb/serve/react/js/components/NotFound/NotFound.test.tsx
@@ -1,8 +1,42 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import NotFound from './NotFound';
+import React from "react";
+import TestRenderer from "react-test-renderer";
+import NotFound from "./NotFound";
+import { configure, shallow, render, mount } from "enzyme";
+import { v4 } from "uuid";
+import {
+  ConfigurationUserInterface,
+  ConfigurationWindow,
+} from "../Configuration/ConfigurationModel";
 
-test('not found includes search term', () => {
-  const search = 'test'
-  const compnent = renderer.create(<NotFound location={ { search : 'test' } }  />)
-})
+import Adapter from "enzyme-adapter-react-16";
+
+configure({ adapter: new Adapter() });
+
+declare let window: ConfigurationWindow;
+
+test("not found includes search term", () => {
+  const uuid = v4();
+  const search = `?query=${uuid}`;
+  const wrapper = mount(<NotFound location={{ search }} />);
+  expect(wrapper.text()).toContain(uuid);
+});
+
+test("not found missing search term", () => {
+  const search = `noquery`;
+  const wrapper = mount(<NotFound location={{ search }} />);
+  expect(wrapper.text()).toContain("empty query");
+});
+
+test("not found includes search term : configured", () => {
+  const uuid = v4();
+  const salt = v4();
+
+  window.config = {
+    userInterface: { notFound: { message_template: `{{query}} : ${salt}` } },
+  };
+
+  const search = `?query=${uuid}`;
+  const wrapper = mount(<NotFound location={{ search }} />);
+  expect(wrapper.text()).toContain(uuid);
+  expect(wrapper.text()).toContain(salt);
+});
--- pheweb/serve/react/js/components/NotFound/NotFound.tsx
+++ pheweb/serve/react/js/components/NotFound/NotFound.tsx
@@ -1,27 +1,34 @@
-import React , { useState, useEffect , useContext } from 'react';
-import {mustacheDiv} from "../../common/Utilities";
-import {useLocation} from "react-router-dom";
-import {RegionContext, RegionState} from "../Region/RegionContext";
-import {ConfigurationUserInterface , ConfigurationWindow} from "../Configuration/ConfigurationModel";
+import React, { useState, useEffect, useContext } from "react";
+import { mustacheDiv } from "../../common/Utilities";
+import { useLocation } from "react-router-dom";
+import { RegionContext, RegionState } from "../Region/RegionContext";
+import {
+  ConfigurationUserInterface,
+  ConfigurationWindow,
+} from "../Configuration/ConfigurationModel";
 
-
-interface Props { location : { search : string } };
-interface QueryResult {};
-
-declare let window : ConfigurationWindow;
-
-const NotFound = (props : Props) => {
-      const { config } = window;
-      const query = new URLSearchParams(props.location.search).get('query');
-      const default_message_template : string = `
-      <p>
-      {{#query}}Could not find page for <i>'{{.}}'</i>{{/query}}
-      {{^query}}An empty query <i>'${query}'</i> was supplied;<br> therefore, a page could not be found.{{/query}}
-      </p>
-      `
-      const message_template : string = config?.userInterface?.notFound?.message_template || default_message_template;
-      const parameters = { query };
-      return mustacheDiv(message_template, parameters);
+interface Props {
+  location: { search: string };
 }
+interface QueryResult {}
 
-export default NotFound
+declare let window: ConfigurationWindow;
+
+const NotFound = (props: Props) => {
+  const { config } = window;
+  const query = new URLSearchParams(props.location.search).get("query");
+  // NOTE: Empty value is set by backtick quotes and not mustache
+  const default_message_template: string = `
+      <p>
+      {{#query}}Could not find page for <i>'<span className="query">{{.}}<span>'</i>{{/query}}
+      {{^query}}An empty query <i className="query">'${query}'</i> was supplied;<br> therefore, a page could not be found.{{/query}}
+      </p>
+      `;
+  const message_template: string =
+    config?.userInterface?.notFound?.message_template ||
+    default_message_template;
+  const parameters = { query };
+  return mustacheDiv(message_template, parameters);
+};
+
+export default NotFound;
