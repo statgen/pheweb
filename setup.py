20d166168480e86a90ce7b6244cdec50b6832cd0
major seitan <majorseitan@gmail.com>
2022-04-05 23:03:39 -0400

    fix rebase

--- setup.py
+++ setup.py
@@ -53,10 +53,7 @@ setup(
         'Flask-Compress~=1.10.1',
         'Flask-Cors~=3.0.10',
         'Flask-Login~=0.5.0',
-<<<<<<< HEAD
         'Werkzeug~=2.0.0',
-=======
->>>>>>> aa62637e (fix:upgrade packages)
         'pyasn1~=0.4.6',
         'google-api-python-client~=2.33.0',
         'google-auth~=2.3.3',
