1b7d4a089b9da5099c0827f7a17ac5165c473cec
major seitan <majorseitan@gmail.com>
2022-08-30 14:24:09 -0400

    switch feature flag for lavaa plot default to on

--- pheweb/serve/react/src/components/Variant/VariantLavaaPlot.tsx
+++ pheweb/serve/react/src/components/Variant/VariantLavaaPlot.tsx
@@ -1,22 +1,22 @@
-import React, { Fragment, useContext } from "react";
-import { LavaaConfiguration, Variant } from "./variantModel";
-import { VariantContext, VariantState } from "./VariantContext";
-import loading from "../../common/Loading";
-import { Lavaa } from "lavaa";
-import { ConfigurationWindow } from "../Configuration/configurationModel";
+import React, { Fragment, useContext } from "react"
+import { LavaaConfiguration, Variant } from "./variantModel"
+import { VariantContext, VariantState } from "./VariantContext"
+import loading from "../../common/Loading"
+import { Lavaa } from "lavaa"
+import { ConfigurationWindow } from "../Configuration/configurationModel"
 
-declare let window: ConfigurationWindow;
-const lavaa : LavaaConfiguration|undefined = window?.config?.userInterface?.variant?.lavaa;
+declare let window: ConfigurationWindow
 
+const defaultLavaaConfiguration : LavaaConfiguration =  { display : true }
+const lavaa : LavaaConfiguration = window?.config?.userInterface?.variant?.lavaa || defaultLavaaConfiguration
 
 interface Props { variantData : Variant.Data }
 
 const VariantLavaaPlot = ({ variantData } : Props) => {
   const { colorByCategory } = useContext<Partial<VariantState>>(VariantContext)
   let result
-  let { display = true } = lavaa;
-  if(display){
-    result = (lavaa && colorByCategory)?<Fragment>
+  if(lavaa.display){
+    result = (colorByCategory)?<Fragment>
       <Lavaa dataprop={variantData.results} colorByCategory={colorByCategory} />
     </Fragment>:loading
   } else {
