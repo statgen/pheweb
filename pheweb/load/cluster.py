a543be48dce4757c31e98438a48d3a708e5702c2
mmaher <mmaher@uger-c012.broadinstitute.org>
2023-04-03 23:44:09 +0000

    add support for engine 'uge' (Univa Grid Engine)

--- pheweb/load/cluster.py
+++ pheweb/load/cluster.py
@@ -12,7 +12,7 @@ from typing import List,Dict,Any
 header_template = {
     'slurm': '''\
 #!/bin/bash
-#SBATCH --array=0-{n_jobs}
+#SBATCH --array=0-{n_jobs_m1}
 #SBATCH --mem=4G
 #SBATCH --time=5-0:0
 #SBATCH --output={tmp_path}/slurm-%j.out
@@ -20,7 +20,15 @@ header_template = {
 ''',
     'sge': '''\
 #!/bin/bash
-#$ -t 0-{n_jobs}
+#$ -t 0-{n_jobs_m1}
+#$ -l h_vmem=4G
+#$ -l h_rt=120:00:00
+#$ -o {tmp_path}
+#$ -e {tmp_path}
+''',
+    'uge': '''\
+#!/bin/bash
+#$ -t 1-{n_jobs}
 #$ -l h_vmem=4G
 #$ -l h_rt=120:00:00
 #$ -o {tmp_path}
@@ -30,19 +38,22 @@ header_template = {
 array_id_variable = {
     'slurm': 'SLURM_ARRAY_TASK_ID',
     'sge': 'SGE_TASK_ID',
+    'uge': '(($SGE_TASK_ID - 1))',
 }
 submit_command = {
     'slurm': 'sbatch',
     'sge': 'qsub',
+    'uge': 'qsub',
 }
 monitor_command = {
     'slurm': 'squeue --long --array --job',
     'sge': 'qstat -j',
+    'uge': 'qstat -j',
 }
 
 def run(argv:List[str]) -> None:
     parser = argparse.ArgumentParser()
-    parser.add_argument('--engine', choices=['slurm', 'sge'], required=True)
+    parser.add_argument('--engine', choices=['slurm', 'sge', 'uge'], required=True)
     parser.add_argument('--step', choices=['parse', 'augment-phenos', 'manhattan', 'qq'], required=True)
     parser.add_argument('--N_per_job', default=5)
     args = parser.parse_args(argv)
@@ -81,7 +92,7 @@ def run(argv:List[str]) -> None:
     tmp_path = get_tmp_path(args.step)
     mkdir_p(tmp_path)
     with open(batch_filepath, 'w') as f:
-        f.write(header_template[args.engine].format(n_jobs = len(jobs)-1, tmp_path=tmp_path))
+        f.write(header_template[args.engine].format(n_jobs_m1 = len(jobs)-1, n_jobs = len(jobs), tmp_path=tmp_path))
         f.write('\n\njobs=(\n')
         for job in jobs:
             f.write(','.join(map(str,job)) + '\n')
